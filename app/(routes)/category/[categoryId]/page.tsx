0fc1aa89452515ac5cdf1fb98fa75c1593df96be
leon004 <lcalderonsast@gmail.com>
2023-09-05 09:39:38 -0600

    buttons for the filter area in categories section

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -1,4 +1,11 @@
+import getCategories from "@/actions/get-categories";
+import getCategory from "@/actions/get-category";
+import getColors from "@/actions/get-colors";
 import getProducts from "@/actions/get-products";
+import getSizes from "@/actions/get-sizes";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+import Filter from "./components/filter";
 
 export const revalidate = 0;
 
@@ -21,12 +28,30 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
         colorId: searchParams.colorId,
         sizeId: searchParams.sizeId
     });
-
+    const sizes = await getSizes();
+    const colors  = await getColors();
+    const category = await getCategory(params.categoryId);
     
 
     return ( 
-        <div>
-            Category
+        <div className="bg-white">
+            <Container>
+                <Billboard 
+                data={category.billboard}
+                />
+                <div className="px-4 sm:px-6 lg:px-8 pb-24">
+                    <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+                        {/* Add Mobile Filters */}
+                        <div className="hidden lg:block">
+                            <Filter 
+                                valueKey="sizeId"
+                                name="Sizes"
+                                data={sizes}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </Container>
         </div>
      );
 }
