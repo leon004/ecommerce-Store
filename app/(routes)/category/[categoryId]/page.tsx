1b0f7c2820cf3c6a88a0544fcef57c8e397a5df6
leon004 <lcalderonsast@gmail.com>
2023-09-05 00:29:17 -0600

    get colors and sizes

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -1,3 +1,5 @@
+import getProducts from "@/actions/get-products";
+
 export const revalidate = 0;
 
 interface CategoryPageProps {
@@ -10,10 +12,18 @@ interface CategoryPageProps {
     }
 }
 
-const CategoryPage: React.FC<CategoryPageProps> = ({
+const CategoryPage: React.FC<CategoryPageProps> = async ({
     params,
     searchParams
 }) => {
+    const products = await getProducts({
+        categoryId: params.categoryId,
+        colorId: searchParams.colorId,
+        sizeId: searchParams.sizeId
+    });
+
+    
+
     return ( 
         <div>
             Category
